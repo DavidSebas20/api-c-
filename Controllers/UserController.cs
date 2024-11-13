55f95cfe30abf4f33e29e224be6f7b86a05daf06
DavidSebas20 <davidsebastian20@outlook.com>
2024-11-13 12:18:23 -0500

    Api created using C# Visual Studio - David Sanguano

---
+++ Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/")]
+        public String saludo()
+        {
+            return "Api C#";
+        }
+    }
+}
